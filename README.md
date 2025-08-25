628a33e92a6c73247253e41f4531505efad738bc
Lee Shui Hong <shlee255@gmail.com>
2025-08-26 00:18:46 +0800

    IR sensor plugin Example

--- README.md
+++ README.md
@@ -14,3 +14,39 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+Create flutter project
+1. Launch VS Code
+Open VS Code or your preferred Code OSS editor.
+
+2. Open the command palette
+Go to View > Command Palette or press Control + Shift + P.
+
+3. Find the Flutter commands
+In the command palette, start typing flutter:. VS Code should surface commands from the Flutter plugin.
+
+4. Run the new project command
+Select the Flutter: New Project command. Your OS or VS Code might ask for access to your documents, agree to continue to the next step.
+
+5. Choose a template
+VS Code should prompt you with Which Flutter template?. Depending on what type of Flutter project you want to create, choose the corresponding template. For a new Flutter app, choose Application.
+
+6. Select a project location
+A file dialog should appear. Select or create the parent directory where you want the project to be created. Don't create the project folder itself, the Flutter tool does so. To confirm your selection, click Select a folder to create the project in.
+
+7. Enter a project name
+VS Code should prompt you to enter a name for your new project. Enter a name for your app that follows the lowercase_with_underscores naming convention, following the Effective Dart guidelines. To confirm your selection, press Enter.
+
+8. Wait for project initialization
+Based on the information you entered, VS Code uses flutter create to bootstrap your app. Progress is often surfaced as a notification in the bottom right and can also be accessed from the Output panel.
+
+9. Run your app
+Your new app should now be created and open in VS Code. To try your new app, follow the steps to run and debug in VS Code.
+
+10. Added permission in the Manifests (\android\app\src\main\AndroidManifest.xml)
+ <uses-permission android:name="android.permission.TRANSMIT_IR" />
+
+11. Add ir_sensor_plugin
+flutter pub add ir_sensor_plugin
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 
 void main() {
@@ -118,5 +119,205 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+*/
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:ir_sensor_plugin/ir_sensor_plugin.dart';
+
+const laskoOnOff =
+    '0000 006D 0000 000C 002E 000E 002E 000E 000E 002E 002E 000E 002E 000E 000E 002E 000E 002E 000E 002E 000E 002E 000E 002E 000E 002E 002E 010D';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  bool _hasIrEmitter = false;
+  String _getCarrierFrequencies = 'Unknown';
+
+  var power = [
+    169,
+    168,
+    21,
+    63,
+    21,
+    63,
+    21,
+    63,
+    21,
+    63,
+    21,
+    63,
+    21,
+    63,
+    21,
+    63,
+    21,
+    1794,
+    169,
+    168,
+    21,
+    21,
+    21,
+    3694
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    bool hasIrEmitter;
+    String getCarrierFrequencies;
+
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await IrSensorPlugin.platformVersion;
+      hasIrEmitter = await IrSensorPlugin.hasIrEmitter;
+      getCarrierFrequencies = await IrSensorPlugin.getCarrierFrequencies;
+    } on PlatformException {
+      platformVersion = 'Failed to get data in a platform.';
+      hasIrEmitter = false;
+      getCarrierFrequencies = 'None';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+      _hasIrEmitter = hasIrEmitter;
+      _getCarrierFrequencies = getCarrierFrequencies;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 15.0,
+              ),
+              Text('Running on: $_platformVersion\n'),
+              Text('Has Ir Emitter: $_hasIrEmitter\n'),
+              Text('IR Carrier Frequencies:$_getCarrierFrequencies'),
+              Container(
+                height: 15.0,
+              ),
+              MaterialButton(
+                color: Colors.amber,
+                onPressed: () async {
+                  if (_hasIrEmitter) {
+                    final String result =
+                        await IrSensorPlugin.transmitListInt(list: power);
+
+                    debugPrint('Emitting  List Int Signal: $result');
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Not has Ir emitter')));
+                  }
+                },
+                child: Text('Transmitt List Int'),
+              ),
+              Container(
+                height: 15.0,
+              ),
+              FormSpecificCode(hasIrEmitter: _hasIrEmitter),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FormSpecificCode extends StatelessWidget {
+  FormSpecificCode({
+    super.key,
+    required this.hasIrEmitter,
+  });
+
+  final bool hasIrEmitter;
+  final _formKey = GlobalKey<FormState>();
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Form(
+          key: _formKey,
+          child: Column(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Write specific String code to transmit',
+                  suffixIcon: IconButton(
+                    onPressed: () => _textController.clear(),
+                    icon: Icon(Icons.clear),
+                  ),
+                ),
+                controller: _textController,
+                validator: (value) {
+                  if (value?.isEmpty ?? true) {
+                    return 'Write the code to transmit';
+                  }
+                  return null;
+                },
+              ),
+            )
+          ])),
+      Container(
+        height: 15.0,
+      ),
+      MaterialButton(
+        color: Colors.amber,
+        onPressed: () async {
+          if (hasIrEmitter && (_formKey.currentState?.validate() ?? false)) {
+            final String result = await IrSensorPlugin.transmitString(
+                pattern: _textController.text);
+            if (result.contains('Emitting') && context.mounted) {
+              ScaffoldMessenger.of(context)
+                ..clearSnackBars()
+                ..showSnackBar(
+                  SnackBar(
+                    content: Text('Broadcasting... ${_textController.text}'),
+                  ),
+                );
+            }
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Not has Ir emitter or invalid code')));
+          }
+        },
+        child: Text('Transmit Specific Code HEX'),
+      )
+    ]);
   }
 }
